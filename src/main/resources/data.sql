0cb21913bd15343895050229109934e7e2a8effb
Nathalie Antoine <n.antoine@live.nl>
2020-09-09 15:12:01 +0200

    changed sql dummy data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -17,3 +17,11 @@ INSERT INTO recipes.recipe VALUES
 (1, 20, 'Flower, Butter, Strawberrys', 30, 'Just do it', 'Strawberry Cake', 8, 1, 3),
 (2, 40, 'Broccoli, Rice, Chicken', 50, 'Asian style', 'Broccoli Dish', 4, 2, 3),
 (3, 20, 'Chips, Frikandel with onions, curry and mayonaise', 30, 'From your local snackbar', 'Hollands glorie', 8, 1, 3);
+
+/* user_id, email, first_name, last_name, password (test)  */
+INSERT INTO recipes.user VALUES
+(1, 'a@', 'a', 'a', '$2a$10$VRCtIl4CVgV5n9CspNQhkOMpz8KrfND5fiGUwlXTKsWSO99zRboqm'),
+(2, 'b@', 'b', 'b', '$2a$10$Zp4Y6iXdx26oxeq8AxH.cOFEtuGZ2IVS9X6GVIoBwDfPxBuY/XKg6');
+
+INSERT INTO recipes.role VALUES
+(1, 'ROLE_USER');
