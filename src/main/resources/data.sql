36b46c795b7aecb5db7889064f279cd72b251a78
Nathalie Antoine <n.antoine@live.nl>
2020-09-09 14:10:06 +0200

    changed sql dummy data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
 INSERT INTO recipes.cuisine VALUES
 (1,'Italian'),
 (2,'Asian'),
-(3,'French'),
+(3,'Dutch'),
 (4,'German'),
 (5,'Spanish');
 
@@ -16,4 +16,4 @@ INSERT INTO recipes.category VALUES
 INSERT INTO recipes.recipe VALUES
 (1, 20, 'Flower, Butter, Strawberrys', 30, 'Just do it', 'Strawberry Cake', 8, 1, 3),
 (2, 40, 'Broccoli, Rice, Chicken', 50, 'Asian style', 'Broccoli Dish', 4, 2, 3),
-(3, 20, 'Chips, Frikandel with onions and mayonaise', 30, 'From your local snackbar', 'Hollands glorie', 8, 1, 3);
+(3, 20, 'Chips, Frikandel with onions, curry and mayonaise', 30, 'From your local snackbar', 'Hollands glorie', 8, 1, 3);
