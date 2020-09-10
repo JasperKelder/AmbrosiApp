e4ac27b258ca2cf130f85980588956fff0cc2dfd
Reinout Smit <reinoutsmit@live.nl>
2020-09-10 13:03:56 +0200

    Created a grid with limit 8 recipes

--- target/classes/data.sql
+++ target/classes/data.sql
@@ -1,7 +1,7 @@
 INSERT INTO recipes.cuisine VALUES
 (1,'Italian'),
 (2,'Asian'),
-(3,'French'),
+(3,'Dutch'),
 (4,'German'),
 (5,'Spanish');
 
@@ -16,12 +16,13 @@ INSERT INTO recipes.category VALUES
 INSERT INTO recipes.recipe VALUES
 (1, 20, 'Flower, Butter, Strawberrys', 30, 'Just do it', 'Strawberry Cake', 8, 1, 3),
 (2, 40, 'Broccoli, Rice, Chicken', 50, 'Asian style', 'Broccoli Dish', 4, 2, 3),
-(3, 20, 'Chips, Frikandel with onions and mayonaise', 30, 'From your local snackbar', 'Hollands glorie', 8, 1, 3);
+(3, 20, 'Chips, Frikandel with onions, curry and mayonaise', 30, 'From your local snackbar', 'Hollands glorie', 8, 1, 3),
 (4, 20, 'Flower, Butter, Strawberrys', 30, 'Just do it', 'Strawberry Cake', 8, 1, 3),
 (5, 40, 'Broccoli, Rice, Chicken', 50, 'Asian style', 'Broccoli Dish', 4, 2, 3),
 (6, 20, 'Chips, Frikandel with onions and mayonaise', 30, 'From your local snackbar', 'Hollands glorie', 8, 1, 3),
 (7, 20, 'Flower, Butter, Strawberrys', 30, 'Just do it', 'Strawberry Cake', 8, 1, 3),
-(8, 40, 'Broccoli, Rice, Chicken', 50, 'Asian style', 'Broccoli Dish', 4, 2, 3);
+(8, 40, 'Broccoli, Rice, Chicken', 50, 'Asian style', 'Broccoli Dish', 4, 2, 3),
+(9, 40, 'Broccoli, Rice, Chicken', 50, 'Asian style', 'Broccoli Dish', 4, 2, 3);
 
 /* user_id, email, first_name, last_name, password (test)  */
 INSERT INTO recipes.user VALUES
