6dc684b38b8124380bf435c196cd464fbdeed50f
Nathalie Antoine <n.antoine@live.nl>
2020-09-15 15:11:42 +0200

    AMP-61 fixed small bug in dubble table in db

--- target/classes/data.sql
+++ target/classes/data.sql
@@ -33,18 +33,18 @@ INSERT INTO recipes.recipe VALUES
 (17, 40, 'Broodje kip', 50, 'Asian style', 'Broodje gehakt', 4, 2, 3),
 (18, 40, 'Maaltijdsalade a la Appie', 50, 'Asian style', 'Maaltijdsalade a la Jumbo', 4, 2, 3);
 
-/* user_id, email, first_name, last_name, password (test)  */
+/* user_id, email, first_name, last_name, password = test  */
 INSERT INTO recipes.user VALUES
 (1, '42@gmail.com', 'Make IT Work', 'Elmo', '$2a$10$VRCtIl4CVgV5n9CspNQhkOMpz8KrfND5fiGUwlXTKsWSO99zRboqm'),
-(2, 'b@', 'b', 'b', '$2a$10$Zp4Y6iXdx26oxeq8AxH.cOFEtuGZ2IVS9X6GVIoBwDfPxBuY/XKg6');
+(2, 'admin', 'Admin', 'Admin', '$2a$10$Zp4Y6iXdx26oxeq8AxH.cOFEtuGZ2IVS9X6GVIoBwDfPxBuY/XKg6');
 
 INSERT INTO recipes.role VALUES
 (1, 'ROLE_USER'),
-(2, 'ROLE_USER');
+(2, 'ROLE_ADMIN');
 
-INSERT INTO recipes.users_roles VALUES
+INSERT INTO recipes.user_roles VALUES
 (1, 1),
-(2, 2);
-
+(2, 2),
+(2, 1);
 /* The table next_val has to be updated (nr of users + 1) in order to make it possible to register new users */
 UPDATE hibernate_sequence SET next_val = 3 WHERE next_val = 1;
