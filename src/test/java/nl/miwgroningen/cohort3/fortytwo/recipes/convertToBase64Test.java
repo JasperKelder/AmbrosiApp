163e507c4d549e21c363420b5493be32169aa4c6
Reinout Smit <reinoutsmit@live.nl>
2020-09-20 19:32:52 +0200

    AMP-56 Created a Junit test and added the h2 database

---
+++ src/test/java/nl/miwgroningen/cohort3/fortytwo/recipes/convertToBase64Test.java
@@ -0,0 +1,11 @@
+package nl.miwgroningen.cohort3.fortytwo.recipes;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class convertToBase64Test {
+
+}
