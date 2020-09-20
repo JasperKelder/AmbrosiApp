751faa17b41316c4a3e04c21f8809b5c2a7c1cf9
JasperKelder <jasperkelder@gmail.com>
2020-09-20 10:42:18 +0200

    Layout and navigation update

--- target/classes/static/script/navigation.js
+++ target/classes/static/script/navigation.js
@@ -8,12 +8,3 @@ function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
 }
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
-}
