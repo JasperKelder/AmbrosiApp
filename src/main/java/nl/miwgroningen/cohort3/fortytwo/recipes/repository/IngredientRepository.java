fdf877fe97144e81f6a9ad8cd92e7e44d535c193
Jasmijn <jasmijnrosalie@gmail.com>
2020-09-22 11:44:49 +0200

    AMP-52 Added IngredientRepository and changed view.html and data.sql

---
+++ src/main/java/nl/miwgroningen/cohort3/fortytwo/recipes/repository/IngredientRepository.java
@@ -0,0 +1,11 @@
+package nl.miwgroningen.cohort3.fortytwo.recipes.repository;
+
+import nl.miwgroningen.cohort3.fortytwo.recipes.model.Ingredient;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * @author Jasper Kelder, Nathalie Antoine, Reinout Smit, Jasmijn van der Veen
+ */
+
+public interface IngredientRepository extends JpaRepository<Ingredient, Integer> {
+}
