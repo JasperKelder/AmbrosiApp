a190458a3cb0dfe1618c83ae75782eef844e8c32
Reinout Smit <reinoutsmit@live.nl>
2020-09-25 13:54:08 +0200

    AMP-67 Added comments

--- src/test/java/nl/miwgroningen/cohort3/fortytwo/recipes/FileUploadTests.java
+++ src/test/java/nl/miwgroningen/cohort3/fortytwo/recipes/FileUploadTests.java
@@ -4,6 +4,14 @@ import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+/**
+ * Note: a test must be written without a type so for example:
+ *
+ * public class and public methods will not be interpreted by Travis
+ * Write all classes without a type like below!
+ *
+ * **/
+
 @SpringBootTest
 class FileUploadTests {
 
