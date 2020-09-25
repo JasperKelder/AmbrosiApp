4da7b995519e8d12faaa56533b8faff16343c02a
Reinout Smit <reinoutsmit@live.nl>
2020-09-25 13:02:56 +0200

    AMP-67 extended FileUploadServiceTest with RecipesApplicationTests

--- src/test/java/nl/miwgroningen/cohort3/fortytwo/recipes/FileUploadServiceTest.java
+++ src/test/java/nl/miwgroningen/cohort3/fortytwo/recipes/FileUploadServiceTest.java
@@ -4,13 +4,15 @@ import nl.miwgroningen.cohort3.fortytwo.recipes.model.Category;
 import nl.miwgroningen.cohort3.fortytwo.recipes.model.Recipe;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
-public class FileUploadServiceTest {
+public class FileUploadServiceTest extends RecipesApplicationTests {
 
     @Test
-    public void convertToBase64WithNullImageTest() {
+    public void mustBeFalse() {
         // Given
         Assert.assertEquals("Yes", "False");
     }
