36ec5e17c07d7eeaeafac43ac61bcdcdd15f5bc7
Reinout Smit <reinoutsmit@live.nl>
2020-09-25 13:35:26 +0200

    AMP-67 Created false test

---
+++ src/test/java/nl/miwgroningen/cohort3/fortytwo/recipes/RecipesApplicationTests.java
@@ -0,0 +1,15 @@
+package nl.miwgroningen.cohort3.fortytwo.recipes;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipesApplicationTests {
+
+    @Test
+    void contextLoads() {
+        Assert.assertEquals("True", "False");
+    }
+
+}
