cea2240076bbf3d29cac47203ec75c3a345d5dd2
Reinout Smit <reinoutsmit@live.nl>
2020-09-25 13:17:10 +0200

    AMP-67 Single test

--- src/test/java/nl/miwgroningen/cohort3/fortytwo/recipes/FileUploadServiceTest.java
+++ src/test/java/nl/miwgroningen/cohort3/fortytwo/recipes/FileUploadServiceTest.java
@@ -9,7 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
-public class FileUploadServiceTest extends RecipesApplicationTests {
+public class FileUploadServiceTest {
 
     @Test
     public void mustBeFalse() {
