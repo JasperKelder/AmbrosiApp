408a5e7635e230708e34cfefbd8660b26582b591
Reinout Smit <reinoutsmit@live.nl>
2020-09-25 13:25:22 +0200

    AMP-67 Made applicationtest public

--- src/test/java/nl/miwgroningen/cohort3/fortytwo/recipes/AmbrosiApplicationTest.java
+++ src/test/java/nl/miwgroningen/cohort3/fortytwo/recipes/AmbrosiApplicationTest.java
@@ -7,10 +7,10 @@ import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AmbrosiApplicationTest {
+public class AmbrosiApplicationTest {
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
 
         Assert.assertNotNull(null);
 
