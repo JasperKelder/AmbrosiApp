e79f50c6000694a26d7e043f846419b76ae14027
JasperKelder <jasperkelder@gmail.com>
2020-10-04 21:16:43 +0200

    Layout update

--- src/main/java/nl/miwgroningen/cohort3/fortytwo/recipes/controller/ChangeEmailAddressController.java
+++ src/main/java/nl/miwgroningen/cohort3/fortytwo/recipes/controller/ChangeEmailAddressController.java
@@ -31,20 +31,15 @@ public class ChangeEmailAddressController {
         this.userService = userService;
     }
 
-    //method to get current user
-    @GetMapping("/changeemailaddress")
-    protected String showUserEmail(Model model, Principal principal) {
-        model.addAttribute("user", userRepository.findByEmailAddress(principal.getName()));
-        return "userinfo";
-    }
+//    //method to get current user
+//    @GetMapping("/changeemailaddress")
+//    protected String showUserEmail(Model model, Principal principal) {
+//        model.addAttribute("user", userRepository.findByEmailAddress(principal.getName()));
+//        return "userinfo";
+//    }
 
     @ModelAttribute("user")
     public EmailChangeDto emailChangeDto() { return new EmailChangeDto(); }
-
-    @GetMapping
-    public String showEmailAddressForm() {
-        return "changeemailaddress";
-    }
 
     //method to save new email address with current user
     @PostMapping
