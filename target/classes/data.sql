057449d2032995636b5410c3e5a6e31373bc38f0
Nathalie Antoine <n.antoine@live.nl>
2020-10-05 13:43:54 +0200

    new demo data

--- target/classes/data.sql
+++ target/classes/data.sql
@@ -48,10 +48,11 @@ INSERT INTO recipes.user_roles VALUES
 /* Has to be after insertion of the values it references (category, cuisine, user)*/
 INSERT INTO recipes.recipe VALUES
 (1, 20, null, 30, 'Just do it', 'Strawberry Cake', 8, 1, 3, 1),
-(2, 40, null, 50, 'Asian style', 'Broccoli Dish', 4, 2, 3, 1),
-(3, 20, null, 30, 'From your local snackbar', 'Hollands glorie', 8, 1, 3, 1),
+(2, 25, null, 5, 'Asian style', 'Cod with chorizo from the oven', 2, 3, 5, 6),
+(3, 40, null, 10, 'Preheat the oven to 180 degrees. Lightly grease a 22 cm tin (20 or 24 is also possible) with some oil. Let the slices of dough thaw and once that''s done, line the mold with it. Meanwhile cut the zucchini into small cubes and the spring onions into rings. Heat some oil in a frying pan and add the zucchini and onion, fry over medium heat. Cut the woody bottoms off the green asparagus and cut them in half. Put the top halves of the asparagus aside for a while, cut the bottom halves into three pieces per half. Add the asparagus pieces to the zucchini and spring onion and fry for another minute or two. Place the eggs in a bowl with the crème fraîche and a good pinch of salt and pepper and stir well. Remove the vegetables from the frying pan (if necessary, pat dry with some kitchen paper if there is still a lot of oil on it) and stir through the mixture as well. Now take the piece of cheese and grate some cheese into the mixture, five tablespoons. Give it a stir. Prick a few holes in the bottom of your lined pan with a fork and pour the mixture into it. Take the remaining halves of the asparagus and divide over the cake. Finish with some more grated cheese, about five tablespoons. Place the cake in the oven and bake for 35-40 minutes until golden brown and done.',
+    'Simple asparagus quiche', 1, 2, 3, 5),
 (4, 60, null, 20, 'Mix the butter and sugar until creamy. This can be done in a small saucepan, in which the butter can be melted over low heat so that it mixes easily. Add the milk and mix until well incorporated. Then add the flour and a pinch of salt and mix into a crumbly dough. Crumble the chocolate chip cookies into chocolate chips and mix well. Now turn 16 small balls from the dough and place them on a plate. Let the balls rest in the refrigerator for 1 hour. Only when the dough balls have rested in the refrigerator long enough, do you melt the chocolate. Put this au bain-marie (in a bowl over a pan of boiling water). Remove the balls from the refrigerator and use a skewer to pass them through the melted chocolate. When you have done this, place the truffles on a parchment-lined plate. Return the truffles to the refrigerator until the chocolate has set.',
-'Cookie dough truffels', 16, 4, 3, 4),
+    'Cookie dough truffels', 16, 4, 3, 4),
 (5, 15, null, 5, 'Prepare the orzo according to the directions on the package. Heat a frying pan and fry the bacon in a dry frying pan for 5 minutes until crispy. Remove the seeds from the pepper and cut into cubes. Fry the bell pepper for the last 2 minutes with the bacon. Stir the bacon, bell pepper, pesto and 75 grams of goat cheese through the orzo and divide between 2 plates. Finish with the rest of the goat cheese.',
     'Orzo with bacon and goat cheese', 2, 3, 1, 7),
 (6, 10, null, 0, 'Slice the olives and quarter the cherry tomatoes. Place half of the basil in a bowl with 3 tablespoons of olive oil and a pinch of pepper and salt and mix well with a hand blender. Mix the lamb''s lettuce with the olives, cherry tomatoes, basil oil and the rest of the basil leaves and finish with the burrata on top.',
@@ -88,9 +89,9 @@ INSERT INTO `recipes`.`ingredient` (`ingredient_id`, `ingredient_name`) VALUES
 (13, 'ground cumin'),
 (14, 'parsley'),
 (15, 'wheat flour'),
-(16, 'Greek  yoghurt'),
+(16, 'Greek yoghurt'),
 (17, 'baking powder'),
-(18, 'Greek  halloumi'),
+(18, 'Greek halloumi'),
 (19, 'pumpkin seeds'),
 (20, 'gnocchi'),
 (21, 'chicken thighs'),
@@ -109,7 +110,17 @@ INSERT INTO `recipes`.`ingredient` (`ingredient_id`, `ingredient_name`) VALUES
 (34, 'red pesto'),
 (35, 'chocolate cookies'),
 (36, 'light brown caster sugar'),
-(37, 'milk chocolate');
+(37, 'milk chocolate'),
+(38, 'peper'),
+(39, 'zucchini'),
+(40, 'spring onions'),
+(41, 'crème fraîche'),
+(42, 'grana padano'),
+(43, 'puff pastry'),
+(44, 'chorizo'),
+(45, 'passata (sifted tomatoes)'),
+(46, 'codfish'),
+(47, 'ciabatta bread');
 
 
 INSERT INTO recipes.recipe_ingredients VALUES
@@ -153,5 +164,19 @@ INSERT INTO recipes.recipe_ingredients VALUES
 (4, 8),
 (4, 35),
 (4, 36),
-(4, 37);
+(4, 37),
+(3, 38),
+(3, 7),
+(3, 39),
+(3, 40),
+(3, 41),
+(3, 42),
+(3, 43),
+(2, 10),
+(2, 11),
+(2, 26),
+(2, 44),
+(2, 45),
+(2, 46),
+(2, 47);
 
