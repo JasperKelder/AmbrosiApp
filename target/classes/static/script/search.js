d00c0cc442cf5bd1df15419748c805eb9dc6b550
Reinout Smit <reinoutsmit@live.nl>
2020-10-06 10:33:29 +0200

    AMP-98 Fixed search function bug

--- target/classes/static/script/search.js
+++ target/classes/static/script/search.js
@@ -1,2 +1,11 @@
-var searchterm = document.getElementById('nav-search').value;
-document.getElementById('searchlink').setAttribute('href', "/searchresults/" + searchterm);
+// function validateInput() {
+//     var inputfield = document.getElementById("nav-search");
+//     if (inputfield === "") {
+//         alert("Name must be filled out");
+//         return false;
+//     }
+//     else {
+//         var searchterm = document.getElementById('nav-search').value;
+//         document.getElementById('searchlink').setAttribute('href', "/searchresults/" + searchterm);
+//     }
+// }
