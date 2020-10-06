66d8df7f32181560e3113149c0a2eefb5f09ce32
Nathalie Antoine <n.antoine@live.nl>
2020-10-06 14:48:11 +0200

    AMP-50 all pages except index,registration and view recipe are secure. (css not working)

--- src/main/java/nl/miwgroningen/cohort3/fortytwo/recipes/configuration/SecurityConfiguration.java
+++ src/main/java/nl/miwgroningen/cohort3/fortytwo/recipes/configuration/SecurityConfiguration.java
@@ -7,13 +7,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
-@EnableWebSecurity
+
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Autowired
@@ -27,11 +26,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers(
+                "/static/**",
                 "/registration**",
-                "/**",
-                "/js/**",
-                "/css/**",
-                "/img/**").permitAll()
+                "/index/**",
+                "/viewrecipe/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
@@ -45,7 +43,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .logoutSuccessUrl("/index?logout")
                 .permitAll();
         http.formLogin().defaultSuccessUrl("/mykitchen", true);
+
     }
+
 
     @Bean
     public DaoAuthenticationProvider authenticationProvider() {
