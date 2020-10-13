007dd03891a0b1bf414cb24b174696743baa47bd
Jasper <jasperkelder@gmail.com>
2020-10-13 16:49:46 +0200

    Search update

--- target/classes/static/script/search.js
+++ target/classes/static/script/search.js
@@ -15,3 +15,15 @@ function validateInput() {
         document.getElementById('searchlink').setAttribute('href', "/searchresults/" + inputfield.value);
     }
 }
+
+var inputfield = document.getElementById("nav-search");
+// Execute a function when the user releases a key on the keyboard
+inputfield.addEventListener("keyup", function(event) {
+    // Number 13 is the "Enter" key on the keyboard
+    if (event.keyCode === 13) {
+        // Cancel the default action, if needed
+        event.preventDefault();
+        // Trigger the button element with a click
+        document.getElementById("searchlink").click();
+    }
+});
