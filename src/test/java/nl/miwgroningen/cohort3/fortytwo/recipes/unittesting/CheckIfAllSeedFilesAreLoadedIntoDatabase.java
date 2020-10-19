051252b2e0ce7796dbb50398189754eb3770b230
Reinout Smit <reinoutsmit@live.nl>
2020-10-19 11:23:48 +0200

    AMP-89 Changed the unit test from seeder.

--- src/test/java/nl/miwgroningen/cohort3/fortytwo/recipes/unittesting/CheckIfAllSeedFilesAreLoadedIntoDatabase.java
+++ src/test/java/nl/miwgroningen/cohort3/fortytwo/recipes/unittesting/CheckIfAllSeedFilesAreLoadedIntoDatabase.java
@@ -9,8 +9,6 @@ import org.mockito.Mock;
 import org.junit.Assert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.test.mock.mockito.MockBeans;
 
 import java.util.List;
 
