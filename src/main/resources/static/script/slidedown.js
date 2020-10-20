6eebf90c75cfc4d89ee7271409a3691ff894341a
Reinout Smit <reinoutsmit@live.nl>
2020-10-20 14:09:22 +0200

    AMP-19 Fixed the active dropdown menu so it stays visible while user is filtering

--- src/main/resources/static/script/slidedown.js
+++ src/main/resources/static/script/slidedown.js
@@ -2,6 +2,19 @@ var coll = document.getElementsByClassName("collapsible");
 var i;
 
 for (i = 0; i < coll.length; i++) {
+    if (window.location.href.indexOf("filterresults") > -1) {
+        function onLoadClick() {
+            coll.className = "collapsible active";
+            var content = coll[i].nextElementSibling;
+            if (content.style.maxHeight){
+                content.style.maxHeight = null;
+            } else {
+                content.style.maxHeight = content.scrollHeight + "px";
+            }
+        }
+        window.onload = onLoadClick();
+    }
+    else {
     coll[i].addEventListener("click", function() {
         this.classList.toggle("active");
         var content = this.nextElementSibling;
@@ -10,5 +23,5 @@ for (i = 0; i < coll.length; i++) {
         } else {
             content.style.maxHeight = content.scrollHeight + "px";
         }
-    });
+    });}
 }
