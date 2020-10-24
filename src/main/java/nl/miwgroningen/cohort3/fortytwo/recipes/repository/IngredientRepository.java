6f592c79ebd7ddcee35c1b3df65abcd899f71d6c
Jasmijn <jasmijnrosalie@gmail.com>
2020-10-24 15:01:58 +0200

    added a custom query getDistinctIngredientNames() to the IngredientRepository, so that the autocompletes shows ingredients only once

--- src/main/java/nl/miwgroningen/cohort3/fortytwo/recipes/repository/IngredientRepository.java
+++ src/main/java/nl/miwgroningen/cohort3/fortytwo/recipes/repository/IngredientRepository.java
@@ -1,7 +1,12 @@
 package nl.miwgroningen.cohort3.fortytwo.recipes.repository;
 
 import nl.miwgroningen.cohort3.fortytwo.recipes.model.Ingredient;
+import nl.miwgroningen.cohort3.fortytwo.recipes.model.Recipe;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+
 import java.util.List;
 import java.util.Optional;
 
@@ -12,5 +17,9 @@ import java.util.Optional;
 
 public interface IngredientRepository extends JpaRepository<Ingredient, Integer> {
     List<Ingredient> findByIngredientName(String ingredientName);
+
+    @Transactional
+    @Query(value = "SELECT DISTINCT ingredient_name FROM recipes.ingredient;", nativeQuery = true)
+    List<String> getDistinctIngredientNames();
 }
 
